235485656b4437481cd76f0057511ae5df964c25
GitPalz <Angadlamba@live.com>
2017-05-11 18:49:10 -0400

    Init Demo Commit
    Hello Git-World!
    Includes a small demo with minor assets. Demo allows you to move string
    around the screen and fades a UI text gives a short control description
    of the demo in and out. Due to simplicity and testing, no game objects
    in this demo build take advantage of Delta Timer.
    
    Some Asset files have been init in this commit.

---
+++ Jade/Assets/Scripts/Demo/MoveDemoText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDemoText : MonoBehaviour {
+    //public float speed = 1.0f;
+    public static bool up = false;
+    public static bool down = false;
+    public static bool left = false;
+    public static bool right = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Up
+        if (Input.GetKeyDown("w")) up = true;
+        if (Input.GetKeyUp("w")) up = false;
+
+        //Down
+        if (Input.GetKeyDown("s")) down = true;
+        if (Input.GetKeyUp("s")) down = false;
+
+        //Left
+        if (Input.GetKeyDown("a")) left = true;
+        if (Input.GetKeyUp("a")) left = false;
+
+        //Right
+        if (Input.GetKeyDown("d")) right = true;
+        if (Input.GetKeyUp("d")) right = false;
+
+        if (up) transform.Translate(Vector2.up); //up
+        if (down) transform.Translate(-Vector2.up); //down
+        if (left) transform.Translate(-Vector2.right); //left
+        if (right) transform.Translate(Vector2.right); //right
+
+    }
+}
---
+++ Jade/Assets/Scripts/Demo/TextBreathing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBreathing : MonoBehaviour
+{
+
+    private float breathCounter;
+    private bool exhale;
+    void Start() { exhale = false; breathCounter = 0; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (exhale) breathCounter -= 0.01f;
+        else        breathCounter += 0.01f;
+
+        if (breathCounter >= 1) exhale = true;
+        else
+        if (breathCounter <= 0) exhale = false;
+
+        //GetComponent<Text>().color.a = breathCounter;
+        var txt = GetComponent<Text>();
+        var newColor = new Color(txt.color.r, txt.color.g, txt.color.b, breathCounter);
+        GetComponent<Text>().color = newColor;
+    }
+}
